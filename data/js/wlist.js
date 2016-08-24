214350f5cf907829034fcc2a73f96cb78d106485
wildskyf <poppin.wildsky+gh@gmail.com>
2016-08-24 23:26:26 +0800

    Refactor

--- data/js/wlist.js
+++ data/js/wlist.js
@@ -1,100 +1,75 @@
+let _ = dom => document.querySelector(dom);
 
-var loadLists = () => new Promise((resolve,reject) => {
-	wlAPI.http.lists.all()
-		.done( (dat,statusCode) => resolve(dat, statusCode) )
-		.fail( (a,b,c) => reject(a,b,c) );
-});
+let updateList = listId =>
+	wlAPI.http.tasks.forList(listId)
+		.done( tasksDat => renderTaskList(tasksDat, listId))
+		.fail( e => console.error(e.stack) );
 
-var loadTasks = ListId => new Promise((resolve,reject) => {
-	wlAPI.http.tasks.forList(ListId)
-		.done( (dat,statusCode) => resolve(dat, statusCode) )
-		.fail( (a,b,c) => reject(a,b,c) );
-});
+let addTask = taskData =>
+	wlAPI.http.tasks.create(taskData)
+		.done( task => {
+			let parser = new DOMParser();
+			let newTaskDoc = parser.parseFromString(`
+				<li class="task" data-task-id="${task.id}"><div>${task.title}</div></li>`, "text/html");
 
-var renderTaskList = (tasksDat, listId) => {
-	var list_content = '';
+			_('#tasklist > ul').insertBefore(newTaskDoc.querySelector('li.task'), _('.task'));
+			_('#newtask-title').value = "";
+		}).fail( e => console.error(e.stack) );
+
+let renderTaskList = (tasksDat, listId) => {
+	let list_content = '';
 	tasksDat.reverse().forEach( task => {
 		list_content += `<li class="task" data-task-id="${task.id}"><div>${task.title}</div></li>`;
 	});
-	document.querySelector('#tasklist > ul').innerHTML = list_content;
-	document.querySelector('#tasklist').dataset["listId"] = listId;
+	_('#tasklist > ul').innerHTML = list_content;
+	_('#tasklist').dataset["listId"] = listId;
 };
 
-var initTaskList = listsDat => {
-	loadTasks(listsDat[0].id)
-		.then( tasksDat => renderTaskList(tasksDat, listsDat[0].id) )
-		.catch( e => console.error(e.stack) );
+let renderListList = listsDat => {
+	let listsSelect = _("#lists_list");
+
+	listsDat.forEach( list => {
+		listsSelect.innerHTML += `<option value="${list.id}">${list.title}</option>`;
+	});
+
+	listsSelect.addEventListener('change', () => {
+		_('#tasklist > ul').innerHTML = `<li><div>Loading...</div></li>`;
+		updateList(listsSelect.value);
+	});
 };
 
-var initListSelect = () => {
-	loadLists()
-		.then( listsDat => {
-			var listsSelect = document.querySelector("#lists_list");
+// ================
 
-			listsDat.forEach( list => {
-				listsSelect.innerHTML += `<option value="${list.id}">${list.title}</option>`;
-			});
-
-			listsSelect.addEventListener('change', () => {
-				document.querySelector('#tasklist > ul').innerHTML = `<li><div>Loading...</div></li>`;
-				loadTasks(listsSelect.value)
-					.then( tasksDat => {
-						renderTaskList(tasksDat, listsSelect.value);
-					})
-					.catch( e => console.error(e) );
-			});
-
-			initTaskList(listsDat);
-	})
-	.catch( (a,b,c) => console.error(a,b,c) );
+let initListAndMenu = () => {
+	wlAPI.http.lists.all()
+		.done( listsDat => {
+			renderListList(listsDat);
+			updateList(listsDat[0].id); // init Task List
+		}).fail( e => console.error(e.stack) );
 };
 
-var initUserInfo = () => {
+let initUserInfo = () => {
 	wlAPI.http.user.all()
 		.done(user => {
-			document.querySelector("#header").innerHTML = `
-				<img src="http://a.wunderlist.com/api/v1/avatar?user_id=${user.id}" alt="user avatar" />`;
-			document.querySelector("#hello").innerHTML = `Hi, ${user.name}, what are you doing today?`;
-		})
-		.fail((a,b,c) => {
-			console.error(a,b,c);
-		})
+			_("#header").innerHTML = `<img src="http://a.wunderlist.com/api/v1/avatar?user_id=${user.id}" alt="user avatar" />`;
+			_("#hello").innerHTML = `Hi, ${user.name}, what are you doing today?`;
+		}).fail( e => console.error(e.stack) );
 };
 
-var initCreateTask = () => {
-	var newtaskInput = document.querySelector('#newtask-title');
-	if(newtaskInput)
-		newtaskInput.addEventListener('keydown', (e) => {
-			if(e.keyCode == 13) {
-				var taskData = {
-					'list_id': parseInt(document.querySelector('#tasklist').dataset["listId"]),
-					'title': document.querySelector('#newtask-title').value
-				};
-				wlAPI.http.tasks.create(taskData)
-					.done((task) => {
-						var newtaskli = document.createElement("li");
-						newtaskli.className = "task";
-						newtaskli.dataset["taskId"] = task.id;
-						var inNewtask = document.createElement("div");
-						inNewtask.innerHTML = task.title;
+let initInputBox = () =>
+	_('#newtask-title').addEventListener('keydown', event => {
+		if(event.keyCode == 13) // when click enter
+			addTask({
+				'list_id': parseInt(_('#tasklist').dataset["listId"]),
+				'title': _('#newtask-title').value
+			});
+	}, false);
 
-						newtaskli.appendChild(inNewtask);
-
-						document.querySelector('#tasklist > ul').insertBefore(newtaskli, document.querySelector('.task'));
-						document.querySelector('#newtask-title').value = "";
-					})
-					.fail((a,b,c) => {
-						console.error(a,b,c);
-					})
-
-			}
-		}, false)
+window.onload = () => {
+	wlAPI.initialized.done( () => {
+		initListAndMenu();
+		initUserInfo();
+		initInputBox();
+	});
 };
-
-wlAPI.initialized.done( () => {
-	initListSelect();
-	initUserInfo();
-});
-
-window.onload = initCreateTask;
 
