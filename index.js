0ae16c23ebe673b2acb22f22a5bc73092137038e
wildskyf <poppin.wildsky+gh@gmail.com>
2017-03-22 23:14:24 +0800

    use browser.identity.launchWebAuthFlow to rewrite OAuth2 flow

--- index.js
+++ index.js
@@ -6,11 +6,48 @@ const redirectURI = 'https://a.wunderlist.com/api/health';
 var local = browser.storage.local;
 var wAccessToken = null;
 
+var validate = url => {
+	// validate the access token
+	var respParts = url && url.split(/\#access_token\=/);
+	var token = respParts && respParts[1];
+
+	if (token) {
+		local.set({'token': token});
+		// success && success();
+		wAccessToken = token;
+		browser.tabs.query({
+			active: true
+		}).then( tabs => {
+			for (let tab of tabs) {
+				browser.tabs.create({
+					url: "data/index.html",
+					windowId: tab.windowId
+				});
+			}
+		});
+	}
+	else {
+		local.remove('token');
+		// failure && failure();
+	}
+}
+
+var authorize = () => {
+	let authURL = authBaseUrl;
+	authURL += `?client_id=${clientID}`;
+	authURL += `&response_type=token`;
+	authURL += `&redirect_uri=${encodeURIComponent(redirectURI)}`;
+
+	return browser.identity.launchWebAuthFlow({
+		interactive: true,
+		url: authURL
+	});
+}
+
+var login() => authorize().then(validate);
+
 local.get().then( data => {
-	if(data.token)
-		wAccessToken = data.token;
-	else
-		login();
+	wAccessToken = data.token || login();
 });
 
 // for Access request from f2e
@@ -21,73 +58,15 @@ function getToken() {
 function removeToken() {
 	local.remove('token');
 	wAccessToken = null;
-    var loginPage = browser.tabs.create({
-        'url': 'https://www.wunderlist.com/logout'
-    }).then( () => {
-		var logoutHandler = (tabId, changeInfo, tabInfo) => {
+	var loginPage = browser.tabs.create({
+		'url': 'https://www.wunderlist.com/logout'
+	}).then( () => {
+		browser.tabs.onUpdated.addListener( (tabId, changeInfo, tabInfo) => {
 			if (tabInfo.url == 'https://www.wunderlist.com/download/') {
 				browser.tabs.remove(tabId);
-				browser.tabs.onUpdated.removeListener(logoutHandler);
+				browser.tabs.onUpdated.removeListener(arguments.callee);
 			}
-		};
-		browser.tabs.onUpdated.addListener(logoutHandler);
-    });
-}
-
-var globalObjects = {
-    'addPanel': undefined,
-    'addButton': undefined
-};
-
-// LOGIN URL composs
-function authParams(state) {
-    return {
-        'client_id': clientID,
-        'response_type': 'token',
-        'redirect_uri': redirectURI
-    };
-}
-
-function login(success, failure) {
-
-	var getAuthUrl = state => {
-		var url = authBaseUrl;
-		var params = authParams(state);
-
-		Object.keys(params)
-			.forEach(function(key, index) {
-				url += (index === 0 ? '?' : '&') + key + '=' + params[key];
-			});
-
-		return url;
-	};
-
-    // Open a new tab as an app tab and do something once it's open.
-    var loginPage = browser.tabs.create({
-        'url': getAuthUrl('foxmosa')
-    }).then( () => {
-		var getTokenHandler = (tabId, changeInfo, tabInfo) => {
-			if ( /^https\:\/\/a\.wunderlist.com\/api\/health/.test(tabInfo.url)) {
-				var url = tabInfo.url;
-				console.log('tab url', url);
-				var respParts = url && url.split(/\#access_token\=/);
-				var token = respParts && respParts[1];
-
-				if (token) {
-					local.set({'token': token});
-					// success && success();
-					wAccessToken = token;
-					browser.tabs.update(tabId, {
-						url:"data/index.html"
-					});
-				} else {
-					local.remove('token');
-					// failure && failure();
-				}
-				browser.tabs.onUpdated.removeListener(getTokenHandler);
-			}
-		};
-		browser.tabs.onUpdated.addListener(getTokenHandler);
-    });
+		});
+	});
 }
 
